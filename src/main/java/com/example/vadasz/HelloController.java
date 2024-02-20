da3423a1986b1fd1f64598449fb6fc9759eb1c99
Bársony József <barsony@kkando.hu>
2024-02-20 15:22:39 +0100

    Reflektor

--- src/main/java/com/example/vadasz/HelloController.java
+++ src/main/java/com/example/vadasz/HelloController.java
@@ -21,6 +21,7 @@ public class HelloController {
 
     private ImageView[][] it = new ImageView[16][32];
     private int[][] t = new int[16][32];
+    private int es=-1, eo=-1;
 
     private int roka = 0;
     private int rokaMax = 0;
@@ -28,9 +29,11 @@ public class HelloController {
     public void initialize() {
         for (int i=0; i<5; i++) icon[i] = new Image(getClass().getResourceAsStream("icons/" + iconNev[i] + ".png"));
         for (int s=0; s<16; s++) for (int o=0; o<32; o++) {
+            int ss=s, oo=o;
             it[s][o] = new ImageView(icon[DARK]);
             it[s][o].setLayoutX(10+o*48);
             it[s][o].setLayoutY(10+s*48);
+            it[s][o].setOnMouseEntered(e -> vilagit(ss, oo));
             pnJatek.getChildren().add(it[s][o]);
         }
         generalErdo();
@@ -43,6 +46,24 @@ public class HelloController {
         }
         rokaMax = roka;
         lbRoka.setText(roka + " / " + rokaMax + " róka");
+    }
+
+    private void vilagit(int s, int o) {
+        if (s!=es || o!=eo) {
+            for (int dS=-2; dS<=+2; dS++) for (int dO=-2; dO<=+2; dO++) {
+                int ss=es+dS, oo=eo+dO;
+                if (ss>=0 && ss<=15 && oo>=0 && oo<=31 && !(Math.abs(dS)==2 && Math.abs(dO)==2)) {
+                    it[ss][oo].setImage(icon[DARK]);
+                }
+            }
+            for (int dS=-2; dS<=+2; dS++) for (int dO=-2; dO<=+2; dO++) {
+                int ss=s+dS, oo=o+dO;
+                if (ss>=0 && ss<=15 && oo>=0 && oo<=31 && !(Math.abs(dS)==2 && Math.abs(dO)==2)) {
+                    it[ss][oo].setImage(icon[t[ss][oo]]);
+                }
+            }
+            es=s; eo=o;
+        }
     }
 
 }
