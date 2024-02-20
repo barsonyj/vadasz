0e50b7e5d17b4afd0131560333a6eb3b301a85c6
Bársony József <barsony@kkando.hu>
2024-02-20 15:51:31 +0100

    Vadaszat

--- src/main/java/com/example/vadasz/HelloController.java
+++ src/main/java/com/example/vadasz/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.vadasz;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -16,15 +17,23 @@ public class HelloController {
     private Image[] icon = new Image[5];
 
     private final int DARK = 0;
+    private final int DEAD = 1;
     private final int ROKA = 2;
+    private final int HOME = 3;
     private final int TREE = 4;
 
     private ImageView[][] it = new ImageView[16][32];
     private int[][] t = new int[16][32];
     private int es=-1, eo=-1;
 
+    private int loves = 0;
+    private int talalt = 0;
     private int roka = 0;
     private int rokaMax = 0;
+
+    private AnimationTimer timer = null;
+    private long most = 0;
+    private long tt = 0;
 
     public void initialize() {
         for (int i=0; i<5; i++) icon[i] = new Image(getClass().getResourceAsStream("icons/" + iconNev[i] + ".png"));
@@ -34,17 +43,27 @@ public class HelloController {
             it[s][o].setLayoutX(10+o*48);
             it[s][o].setLayoutY(10+s*48);
             it[s][o].setOnMouseEntered(e -> vilagit(ss, oo));
+            it[s][o].setOnMousePressed(e -> loves(ss, oo));
             pnJatek.getChildren().add(it[s][o]);
         }
         generalErdo();
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                most = now;
+                if (now > tt) elbujik();
+            }
+        };
+        timer.start();
     }
 
     private void generalErdo() {
-        roka = 0;
+        roka = 0; loves = 0; talalt = 0;
         for (int s=0; s<16; s++) for (int o=0; o<32; o++) {
             if (Math.random() < 0.1) { t[s][o] = ROKA; roka++; } else t[s][o] = TREE;
         }
         rokaMax = roka;
+        lbLoves.setText(loves + " lövés / " + talalt + " találat");
         lbRoka.setText(roka + " / " + rokaMax + " róka");
     }
 
@@ -63,7 +82,28 @@ public class HelloController {
                 }
             }
             es=s; eo=o;
+            tt = most + 500_000_000; // ns
         }
+    }
+
+    private void elbujik() {
+        for (int dS=-2; dS<=+2; dS++) for (int dO=-2; dO<=+2; dO++) {
+            int ss=es+dS, oo=eo+dO;
+            if (ss>=0 && ss<=15 && oo>=0 && oo<=31 && !(Math.abs(dS)==2 && Math.abs(dO)==2) && t[ss][oo] == ROKA) {
+                it[ss][oo].setImage(icon[HOME]); t[ss][oo] = HOME; roka--;
+            }
+        }
+        lbRoka.setText(roka + " / " + rokaMax + " róka");
+    }
+
+    private void loves(int s, int o) {
+        loves++;
+        if (t[s][o] == ROKA) {
+            it[s][o].setImage(icon[DEAD]); t[s][o] = DEAD; roka--;
+            lbRoka.setText(roka + " / " + rokaMax + " róka");
+            talalt++;
+        }
+        lbLoves.setText(loves + " lövés / " + talalt + " találat");
     }
 
 }
