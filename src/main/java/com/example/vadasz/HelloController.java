ca1284bd8a4a65dcf229b5db3cfef9c64b166876
Bársony József <barsony@kkando.hu>
2024-02-20 14:53:56 +0100

    Random

--- src/main/java/com/example/vadasz/HelloController.java
+++ src/main/java/com/example/vadasz/HelloController.java
@@ -16,9 +16,14 @@ public class HelloController {
     private Image[] icon = new Image[5];
 
     private final int DARK = 0;
+    private final int ROKA = 2;
+    private final int TREE = 4;
 
     private ImageView[][] it = new ImageView[16][32];
     private int[][] t = new int[16][32];
+
+    private int roka = 0;
+    private int rokaMax = 0;
 
     public void initialize() {
         for (int i=0; i<5; i++) icon[i] = new Image(getClass().getResourceAsStream("icons/" + iconNev[i] + ".png"));
@@ -28,6 +33,16 @@ public class HelloController {
             it[s][o].setLayoutY(10+s*48);
             pnJatek.getChildren().add(it[s][o]);
         }
+        generalErdo();
+    }
+
+    private void generalErdo() {
+        roka = 0;
+        for (int s=0; s<16; s++) for (int o=0; o<32; o++) {
+            if (Math.random() < 0.1) { t[s][o] = ROKA; roka++; } else t[s][o] = TREE;
+        }
+        rokaMax = roka;
+        lbRoka.setText(roka + " / " + rokaMax + " róka");
     }
 
 }
